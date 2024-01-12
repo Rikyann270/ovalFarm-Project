b2c8de1571f93f31da52949c16cae440f7e935dd
Rikyann270 <rikyann270@gmail.com>
2024-01-12 08:23:00 -0800

    debug 1

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Chickspage from './products/Chickspage.jsx'
 import Datepage from './date/Datepage.jsx'
-// import './index.css'
+
 import {
   createBrowserRouter,
   RouterProvider,
@@ -15,18 +15,17 @@ const router = createBrowserRouter([
     element: <App/>,
   },
     {
-    path: "chicks",
+    path: "/chicks",
     element: <Chickspage/>,
   },
   {
-    path: "date_confermation",
+    path: "/date_confermation",
     element: <Datepage/>,
   },
 ]);
 
 const root=ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
     <RouterProvider router = {router} />
-  </React.StrictMode>,
+
 )
